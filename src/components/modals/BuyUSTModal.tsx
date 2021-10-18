de69754b123d3f7f2b06ae4c21e78ad71cc1932f
Victor <victor@terra.money>
2021-10-18 10:46:46 +0900

    Fix as required
    * Remove icon-close.svg, icon-extension.svg
    * Remove unnecessary <>
    * Link query string with qs

--- src/components/modals/BuyUSTModal.tsx
+++ src/components/modals/BuyUSTModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import qs from 'qs';
 
 import BasicModal from '../common/BasicModal';
 import bitfinex from '../../assets/bitfinex.svg';
@@ -40,13 +41,24 @@ const exchangeResources: Resource[] = [
 ];
 
 const getFiatResources = (transakApiKey?: string): Resource[] => {
+  const link = `https://global.transak.com/?${qs.stringify(
+    {
+      apiKey: transakApiKey || null,
+      cryptoCurrencyList: 'UST,LUNA',
+      defaultCryptoCurrency: 'UST',
+      networks: 'terra',
+    },
+    {
+      skipNulls: true,
+      encode: false,
+    },
+  )}`;
+
   return [
     {
       name: 'Transak',
       image: transak,
-      link: `https://global.transak.com/?cryptoCurrencyList=UST,LUNA&defaultCryptoCurrency=UST&networks=terra${
-        transakApiKey ? `&apiKey=${transakApiKey}` : ''
-      }`,
+      link,
     },
   ];
 };
@@ -61,38 +73,36 @@ interface Props {
 const BuyUSTModal = ({ transakApiKey, onClose, open, title }: Props) => {
   const withFiatResources = getFiatResources(transakApiKey);
   return (
-    <>
-      <BasicModal onClose={onClose} open={open} title={title}>
-        <>
-          <section className={styles.exchange}>
-            <p className={styles.subtitle}>Exchanges</p>
-            <div className={styles.cardList}>
-              {exchangeResources.map((resource: Resource, i: number) => {
-                return (
-                  <Card
-                    key={`exchange-resource-${i.toString()}`}
-                    resource={resource}
-                  />
-                );
-              })}
-            </div>
-          </section>
-          <section>
-            <p className={styles.subtitle}>With Fiat</p>
-            <div className={styles.cardList}>
-              {withFiatResources.map((resource: Resource, i: number) => {
-                return (
-                  <Card
-                    key={`fiat-resource-${i.toString()}`}
-                    resource={resource}
-                  />
-                );
-              })}
-            </div>
-          </section>
-        </>
-      </BasicModal>
-    </>
+    <BasicModal onClose={onClose} open={open} title={title}>
+      <>
+        <section className={styles.exchange}>
+          <p className={styles.subtitle}>Exchanges</p>
+          <div className={styles.cardList}>
+            {exchangeResources.map((resource: Resource, i: number) => {
+              return (
+                <Card
+                  key={`exchange-resource-${i.toString()}`}
+                  resource={resource}
+                />
+              );
+            })}
+          </div>
+        </section>
+        <section>
+          <p className={styles.subtitle}>With Fiat</p>
+          <div className={styles.cardList}>
+            {withFiatResources.map((resource: Resource, i: number) => {
+              return (
+                <Card
+                  key={`fiat-resource-${i.toString()}`}
+                  resource={resource}
+                />
+              );
+            })}
+          </div>
+        </section>
+      </>
+    </BasicModal>
   );
 };
 
